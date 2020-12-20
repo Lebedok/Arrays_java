8706936b016b51fe52fce44cfc3f831afeb64922
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-19 23:55:58 -0600

    Add files via upload

---
+++ ArraySorting.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+
+        long [] numbers = {10, 5, 11, 3, 45, 34};
+
+        // sorting
+        System.out.println(Arrays.toString(numbers));
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+
+        // Sorting names
+        String [] names = {"Olivia", "David", "Dan", "Priyanka", "Gopal"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+
+        String [] nums = {"100", "1", "10","11", "10100" };
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+
+
+
+
+            }
+        }
+
