24fc5e222dca11d861407982f4287be862e3bbe7
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-21 20:07:21 -0600

    Add files via upload

---
+++ BiggerNumberArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class BiggerNumberArray {
+    public static void main(String[] args) {
+
+        int[] numbers = {6, 45, 367, 23, 97};
+        // int biggest = integer.MIN_VALUE;
+        int biggestNum = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > biggestNum) {
+                biggestNum = numbers[i];
+
+            }
+        }
+        System.out.println(biggestNum);
+    }
+}
