74b05b472677534a7c2fa52b6863a8fcb8cc61da
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-28 20:23:08 -0600

    Add files via upload

---
+++ ObjectArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class ObjectArray {
+    public static void main(String[] args) {
+
+        // OBJECT: we have one object class in java, this object class is mother of all object in java.
+
+
+        Object [] values = {1, "Techtorial", 4.5, true, "Chicago"};
+
+        for (Object value : values){
+            System.out.println(value);
+        }
+
+
+    }
+}
---
+++ PrintNumbers.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+
+        char [] ch = {'a','*','-', 'r', '4', '2','%', 'w', '8' };
+
+
+        for (int i = 0; i <= ch.length; i++){
+
+
+
+            }
+        }
+
+
+
+    }
+
